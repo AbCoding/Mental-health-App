f7e494f50b600e005aeb10273aae2c80b3329ed8
ericg <ericgungyang@gmail.com>
2021-10-16 22:12:17 -0500

    changed the box names to options (doesnt rlly work since i dont have all the numbers and stuff)

--- app/src/main/java/com/example/mental_health_app2/HelpOptions.kt
+++ app/src/main/java/com/example/mental_health_app2/HelpOptions.kt
@@ -12,23 +12,23 @@ class HelpOptions : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_help_options)
-        val text= findViewById<TextView>(R.id.SadV)
+        val text = findViewById<TextView>(R.id.SadV)
         val extras = intent.extras
-        val sliderValue= extras?.getFloat("sliderValue")
-        val option=13
-        var MyHelper= DBHelper(applicationContext)
-        var db= MyHelper.readableDatabase
+        val sliderValue = extras?.getFloat("sliderValue")
+        val option = 13
+        var MyHelper = DBHelper(applicationContext)
+        var db = MyHelper.readableDatabase
         val cal = Calendar.getInstance()
         val doy = cal[Calendar.DAY_OF_YEAR]
-        var cv= ContentValues()
-        cv.put("DAY",doy )
-        cv.put("HAPPINESS",sliderValue )
-        cv.put("OPTION",option )
-        cv.put("DIARY","hellwewewo" )
-        db.insert("USERHAPPINESS",null,cv)
+        var cv = ContentValues()
+        cv.put("DAY", doy)
+        cv.put("HAPPINESS", sliderValue)
+        cv.put("OPTION", option)
+        cv.put("DIARY", "hellwewewo")
+        db.insert("USERHAPPINESS", null, cv)
         val Back = findViewById<Button>(R.id.SadBack)
-        Back?.setOnClickListener(){
-            val intent= Intent(this,MainActivity::class.java)
+        Back?.setOnClickListener() {
+            val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
             finish()
         }
@@ -37,55 +37,81 @@ class HelpOptions : AppCompatActivity() {
             val intent = Intent(this, option1::class.java)
             startActivity(intent)
             finish()
-        }
 
-            fun SafeDeleteStr(arr: MutableList<String>,arg:String){
-                if(arr.size > 1){
-                    arr.remove(arg)
+            val Option2Button = findViewById<Button>(R.id.option2button)
+            Option1Button?.setOnClickListener() {
+                val intent = Intent(this, option1::class.java)
+                startActivity(intent)
+                finish()
+
+                val Option3Button = findViewById<Button>(R.id.Option3button)
+                Option1Button?.setOnClickListener() {
+                    val intent = Intent(this, option1::class.java)
+                    startActivity(intent)
+                    finish()
                 }
-            }
-            fun outF(feel: Float) {
-                val arr = mutableListOf(0.3,1.2,3.0,3.1,3.2,3.8)
-                val arrwords = mutableListOf("lool sad dafuh","ugly ass","suck cock","cock csuc","22321","oefuboaubefoabe")
-                val templist: MutableList<Double> = mutableListOf()
-                val templist1: MutableList<String> = mutableListOf()
-                for ((i, e) in arr.withIndex()) {
-                    val specify = 0.5
 
-                    val help1 = feel-specify
-                    val help2 = specify+feel
-                    if (help1<e) {
-                        if (e<help2){
-                            templist.add(e)
-                            templist1.add(arrwords.elementAt(i))
+                fun SafeDeleteStr(arr: MutableList<String>, arg: String) {
+                    if (arr.size > 1) {
+                        arr.remove(arg)
+                    }
+                }
+
+                fun outF(feel: Float) {
+                    val arr = mutableListOf(0.3, 1.2, 3.0, 3.1, 3.2, 3.8)
+                    val arrwords = mutableListOf(
+                        "lool sad dafuh",
+                        "ugly ass",
+                        "suck cock",
+                        "cock csuc",
+                        "22321",
+                        "oefuboaubefoabe"
+                    )
+                    val templist: MutableList<Double> = mutableListOf()
+                    val templist1: MutableList<String> = mutableListOf()
+                    for ((i, e) in arr.withIndex()) {
+                        val specify = 0.5
+
+                        val help1 = feel - specify
+                        val help2 = specify + feel
+                        if (help1 < e) {
+                            if (e < help2) {
+                                templist.add(e)
+                                templist1.add(arrwords.elementAt(i))
+
+
+                            }
 
 
                         }
 
-
                     }
+                    val option1: MutableList<String> = mutableListOf()
+                    option1.add(templist1.random())
+                    SafeDeleteStr(templist1, option1[0])
+
+
+                    option1.add(templist1.random())
+                    SafeDeleteStr(templist1, option1[1])
+
+
+                    option1.add(templist1.random())
+                    SafeDeleteStr(templist1, option1[2])
+
+
+                    Option1Button.setText(option1[0])
+                    Option2Button.setText(option1[1])
+                    Option3Button.setText(option1[1])
+
 
                 }
-                val option1: MutableList<String> = mutableListOf()
-                option1.add(templist1.random())
-                SafeDeleteStr(templist1,option1[0])
-
-
-                option1.add(templist1.random())
-                SafeDeleteStr(templist1,option1[1])
-
-
-                option1.add(templist1.random())
-                SafeDeleteStr(templist1,option1[2])
-
+                if (sliderValue != null) {
+                    outF(sliderValue)
+                }
 
 
             }
-        if (sliderValue != null) {
-            outF(sliderValue)
-        }
-
 
         }
-
     }
+}
