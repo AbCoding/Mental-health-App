5767e37914ab81565e21e18d6d10e40b01daeb8a
AbCoding <pandeabhinav06@gmail.com>
2021-10-16 21:45:57 -0500

    Added Slider

--- app/src/main/java/com/example/mental_health_app2/HelpOptions.kt
+++ app/src/main/java/com/example/mental_health_app2/HelpOptions.kt
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
 
 class HelpOptions : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -38,7 +39,11 @@ class HelpOptions : AppCompatActivity() {
             finish()
         }
 
-
+            fun SafeDeleteStr(arr: MutableList<String>,arg:String){
+                if(arr.size > 1){
+                    arr.remove(arg)
+                }
+            }
             fun outF(feel: Float) {
                 val arr = mutableListOf(0.3,1.2,3.0,3.1,3.2,3.8)
                 val arrwords = mutableListOf("lool sad dafuh","ugly ass","suck cock","cock csuc","22321","oefuboaubefoabe")
@@ -63,23 +68,23 @@ class HelpOptions : AppCompatActivity() {
                 }
                 val option1: MutableList<String> = mutableListOf()
                 option1.add(templist1.random())
-                templist1.remove(option1[0])
-                println(option1)
-                val option2: MutableList<String> = mutableListOf()
-                option2.add(templist1.random())
-                templist1.remove(option2[0])
-                println(option2)
-                val option3: MutableList<String> = mutableListOf()
-                option3.add(templist1.random())
-                templist1.remove(option3[0])
-                println(option3)
+                SafeDeleteStr(templist1,option1[0])
+
+
+                option1.add(templist1.random())
+                SafeDeleteStr(templist1,option1[1])
+
+
+                option1.add(templist1.random())
+                SafeDeleteStr(templist1,option1[2])
+
 
 
             }
-
         if (sliderValue != null) {
             outF(sliderValue)
         }
+
 
         }
 
