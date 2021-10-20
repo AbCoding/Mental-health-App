463ab17ee79da9d05c8c15b48a6921edd3f23cbf
ericg <ericgungyang@gmail.com>
2021-10-19 20:27:30 -0500

    added diary and stuff

--- app/src/main/java/com/example/mental_health_app2/option1.kt
+++ app/src/main/java/com/example/mental_health_app2/option1.kt
@@ -1,14 +1,34 @@
 package com.example.mental_health_app2
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+
 import android.widget.TextView
+import android.widget.Toast
+import com.google.android.material.textfield.TextInputLayout
 
 class option1 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_option1)
+        val option1text = findViewById<TextView>(R.id.option1text)
+        val extras = intent.extras
+        val Options = extras?.getString("Options")
+        option1text.setText(Options)
+
+
+
+        val Back = findViewById<Button>(R.id.Option1Back)
+        Back?.setOnClickListener() {
+            val intent = Intent(this, HelpOptions::class.java)
+            startActivity(intent)
+            finish()
+
+
     }
 
+}
 
 }
